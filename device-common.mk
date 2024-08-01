ad08ed294b59ac6f63112045fe70b66c7e73e7e1
Michael Gernoth <michael@gernoth.net>
2024-08-01 22:46:36 +0200

    b4s4: Remove unwanted packages
    Change-Id: I5345fc03f45844bb10d629f100720e39fbbdf9ee

--- device-common.mk
+++ device-common.mk
@@ -114,6 +114,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Set thermal warm reset
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.thermal_warmreset = true \
