897028e445d4aecb91eb61d471dbe808e3a7bae7
Onelots <54822802+Oneloutre@users.noreply.github.com>
2024-08-08 00:23:56 +0200

    b4s4: PICO_GAPPS no more needed + remove Lineage's useless code

--- device-sargo.mk
+++ device-sargo.mk
@@ -18,9 +18,6 @@ PRODUCT_HARDWARE := sargo
 
 include device/google/bonito/device-common.mk
 
-# New try using Pico Gapps and Velvet
-TARGET_USES_PICO_GAPPS := true
-
 DEVICE_PACKAGE_OVERLAYS += \
     device/google/bonito/sargo/overlay \
     device/google/bonito/sargo/overlay-evolution
