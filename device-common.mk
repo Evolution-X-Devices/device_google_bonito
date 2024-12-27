67631b1c8445d21a4defdf5c17fc70041693d787
Onelots <onelots@onelots.fr>
2024-12-27 16:04:20 +0100

    b4s4: define res of bootanimations to 1080p
    Signed-off-by: Onelots <onelots@onelots.fr>

--- device-common.mk
+++ device-common.mk
@@ -30,6 +30,9 @@ $(call inherit-product, vendor/google/GoogleCamera/b4s4/config.mk)
 # Device now uses mini_gapps
 TARGET_USES_MINI_GAPPS := true
 
+# Maybe will fix bootanimations ?
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Bug 77867216
 PRODUCT_PROPERTY_OVERRIDES += audio.adm.buffering.ms=3
 PRODUCT_PROPERTY_OVERRIDES += vendor.audio.adm.buffering.ms=3
